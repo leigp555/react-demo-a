70e333696d452bcbbc32059009acac2adc51ba63
leigp <907090585@qq.com>
2021-10-20 23:54:11 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 const root = document.querySelector("#root");
 let n = 0;
-const Demo =()=>React.createElement("div", { className: "red", key: "fuck" }, [
-  n,
-  React.createElement(
-    "button",
-    {
-      key: "fuckYou",
-      onClick: () => {
-        n += 1;
-        ReactDOM.render(Demo(), root);
+const Demo = 
+  React.createElement("div", { className: "red", key: "fuck" }, [
+    n,
+    React.createElement(
+      "button",
+      {
+        key: "fuckX",
+        onClick: () => {
+          n += 1;
+          ReactDOM.render(Demo, root);
+         
+        },
       },
-    },
-    "+1"
-  ),
-]) ;
-ReactDOM.render(Demo(), root);
+      "+1"
+    ),
+  ]);
+   console.log(n)
+ReactDOM.render(Demo, root);
