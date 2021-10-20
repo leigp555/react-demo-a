e07706769c5d7861951943d9ca3fb179806a8e25
leigp <907090585@qq.com>
2021-10-20 22:51:32 +0800

    react button+1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+const root = document.querySelector("#root");
+let n = 0;
+const Demo =()=>React.createElement("div", { className: "red", key: "fuck" }, [
+  n,
+  React.createElement(
+    "button",
+    {
+      key: "fuckYou",
+      onClick: () => {
+        n += 1;
+        ReactDOM.render(Demo(), root);
+      },
+    },
+    "+1"
+  ),
+]) ;
+ReactDOM.render(Demo(), root);
